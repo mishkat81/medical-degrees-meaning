0a83ef8db6f57db40331e5a0cb7b92100b537550
Mishkat Ahmed <72969563+mishkat81@users.noreply.github.com>
2021-01-24 00:36:38 +0600

    Medical degrees meaning here
    This app creating by JavaScript

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const myAbbreviation=[
+     {'word':'mbbs', 'meaning':'Bachelor of medicine, bachelor of surgery'},
+     {'word':'fcps', 'meaning':'Fellow of the College of Physicians and Surgeons'},
+     {'word':'mcps', 'meaning':'Member of College of Physicians and Surgeons'},
+     {'word':'frcs', 'meaning':'Fellowship of the Royal Colleges of Surgeons'},
+     {'word':'mrcp', 'meaning':'Membership of the Royal Colleges of Physicians of the United Kingdom'},
+     {'word':'d.l.o', 'meaning':'Diploma in Otorhinolaryngology'},
+     {'word':'d.ortho', 'meaning':'Diploma in Orthopedic Surgery'},
+     {'word':'d.card', 'meaning':'Diploma in Cardiology'},
+     {'word':'ddv', 'meaning':'Diploma in Dermatology & Venereology'},
+     {'word':'dco', 'meaning':'Diploma in Community Ophthalmology'},
+     {'word':'dtcd', 'meaning':'Diploma in Tuberculosis and Chest Diseases'},
+     {'word':'dgo', 'meaning':'Diploma in Gynecology and Obstetrics'},
+     {'word':'md', 'meaning':'Doctor of Medicine'},
+]
+
+function myFunc(data){
+
+let myDiv = document.getElementById("rowID");
+myDiv.innerHTML = '';
+
+for(let i = 0; i < data.length; i++){
+ 
+ let myRow = `<div class="col-lg-3 col-md-6">
+                 <div class="main-part">
+                         <h5>${data[i].word}</h5>
+                         <h6>${data[i].meaning}</h6>
+                    </div>
+                 </div>` 
+
+             myDiv.innerHTML += myRow;
+}
+
+
+}
+myFunc(myAbbreviation);
